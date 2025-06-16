ae6abfe1cbd38c24fded18e961338d0d5535b46e
StephenM <zmmh2430@leeds.ac.uk>
2025-06-16 22:11:30 +0100

    Prevent polymorph reversal on dead NPCs to avoid ghost graphics

--- src/map/map/events/timed_polymorph.rs
+++ src/map/map/events/timed_polymorph.rs
@@ -16,7 +16,7 @@ impl Map {
         let mut npcs_to_reverse: Vec<(i32, i32, i32, Coords, Direction)> = Vec::new();
         
         for (npc_index, npc) in &self.npcs {
-            if npc.polymorphed {
+            if npc.polymorphed && npc.alive {
                 npcs_to_reverse.push((*npc_index, npc.old_id, npc.id, npc.coords, npc.direction));
             }
         }
@@ -36,28 +36,31 @@ impl Map {
             );
 
             if let Some(npc) = self.npcs.get_mut(&npc_index) {
-                npc.id = old_id; 
-                npc.polymorphed = false; 
-                npc.old_id = 0;  
-                
-                if let Some(original_data) = NPC_DB.npcs.get(old_id as usize - 1) {
-                    npc.max_hp = original_data.hp;
-                    npc.hp = original_data.hp; 
+                // Double-check the NPC is still alive and polymorphed before reversing
+                if npc.alive && npc.polymorphed {
+                    npc.id = old_id; 
+                    npc.polymorphed = false; 
+                    npc.old_id = 0;  
+                    
+                    if let Some(original_data) = NPC_DB.npcs.get(old_id as usize - 1) {
+                        npc.max_hp = original_data.hp;
+                        npc.hp = original_data.hp; 
+                    }
+
+                    self.send_packet_all(
+                        PacketAction::Agree,
+                        PacketFamily::Npc,
+                        NpcAgreeServerPacket {
+                            npcs: vec![NpcMapInfo {
+                                index: npc_index,
+                                id: old_id,
+                                coords,
+                                direction,
+                            }],
+                        },
+                    );
                 }
-            } 
-            
-            self.send_packet_all(
-                PacketAction::Agree,
-                PacketFamily::Npc,
-                NpcAgreeServerPacket {
-                    npcs: vec![NpcMapInfo {
-                        index: npc_index,
-                        id: old_id,
-                        coords,
-                        direction,
-                    }],
-                },
-            );
+            }
         }
     }
 }
